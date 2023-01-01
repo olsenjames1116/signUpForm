70525ff4414961f0fb5b5135b5da6316da9b21a9
James Olsen <olsenjames1116@gmail.com>
2023-01-01 14:17:32 -0500

    Add password confirmation
    The entered passwords for both the password field and confirm password
    field are evaluated against each other to see if they match. The form
    will not submit until they match.

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const submitButton = document.querySelector('button[type="submit"]');
+const password = document.querySelector('input#password');
+const confirmPassword = document.querySelector('input#confirmPassword');
+const matchError = document.querySelector('input+p')
+
+console.log(password.value);
+console.log(confirmPassword.value);
+
+function checkPasswordsMatch(){
+    if(confirmPassword.value !== password.value){
+        matchError.textContent = '*Passwords do not match';
+        console.log(password.value);
+        console.log(confirmPassword.value);
+        return false;
+    }
+    else{
+        matchError.textContent = '';
+        console.log(password.value);
+        console.log(confirmPassword.value);
+        return true;
+    }
+}
