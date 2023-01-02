568d62461845a43cfc3694d956799cad2f0dda81
James Olsen <olsenjames1116@gmail.com>
2023-01-01 21:15:58 -0500

    Format button and update password confirmation
    The submit button was given some styling and formatted to be more
    visually appealing on the page.
    
    The password confirmation function was updated to change the background
    color of both password inputs to give the user a better visual cue that
    the passwords did not match desired input.

--- javascript.js
+++ javascript.js
@@ -3,20 +3,15 @@ const password = document.querySelector('input#password');
 const confirmPassword = document.querySelector('input#confirmPassword');
 const matchError = document.querySelector('input#password+p')
 
-console.log(password.value);
-console.log(confirmPassword.value);
-
 function checkPasswordsMatch(){
     if(confirmPassword.value !== password.value){
         matchError.textContent = '*Passwords do not match';
-        console.log(password.value);
-        console.log(confirmPassword.value);
+        confirmPassword.setAttribute('style','background-color: rgb(229, 155, 155);');
+        password.setAttribute('style','background-color: rgb(229, 155, 155);');
         return false;
     }
     else{
         matchError.textContent = '';
-        console.log(password.value);
-        console.log(confirmPassword.value);
         return true;
     }
 }
